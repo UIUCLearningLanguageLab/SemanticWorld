4f5bc9e4dad4c9d13e531f8122d6d80dfed6f0e4
Jon Willits <jwillits@illinois.edu>
2021-10-12 09:07:17 -0500

    init drive system

--- src/livingthings/drive_system.py
+++ src/livingthings/drive_system.py
@@ -1,4 +1,20 @@
+import numpy as np
+
 class DriveSystem:
 
-	def __init__(self):
-		pass
+	def __init__(self, drive_dict):
+		self.num_drives = 0
+		self.drive_label_list = []
+		self.drive_index_dict = {}
+		self.drive_array = None
+	
+
+	def init_drive_system(self, drive_dict):
+		self.drive_array = np.zeros([len(drive_dict)])
+
+		for drive in drive_dict:
+			self.drive_label_list.append(drive)
+			self.drive_index_dict[drive] = self.num_drives
+			self.num_drives += 1
+
+
