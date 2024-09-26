66dd5a369310455cfca70059cfb64c48b4bc57a6
Jon Willits <jwillits@illinois.edu>
2024-09-26 13:46:51 -0500

    clear project

--- SemanticWorld.py
+++
@@ -1,9 +0,0 @@
-from src.display import display
-from src import world
-
-if __name__ == "__main__" :
-
-    the_world = world.World()
-
-    for i in range(30):
-        the_world.next()
--- config/display_cfg.py
+++
@@ -1,4 +0,0 @@
-class Display:
-
-	height = 1200
-	width = 1200
